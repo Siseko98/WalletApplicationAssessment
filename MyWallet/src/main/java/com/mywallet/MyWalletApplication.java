f5128e17cde6bc3a80d1bc1a769a8114fb0caf3e
Siseko98 <167862784+Siseko98@users.noreply.github.com>
2024-05-07 14:33:30 +0200

    Initial commit

---
+++ MyWallet/src/main/java/com/mywallet/MyWalletApplication.java
@@ -0,0 +1,15 @@
+package com.mywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWalletApplication.class, args);
+	}
+
+
+
+}
